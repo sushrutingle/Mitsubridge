e0de819ab5eab73e1a4cf81af408ca9cefd8e070
sushrutingle <sushrutingle7@gmail.com>
2026-08-02 14:57:15 +0530

    feat: wire homepage to Sanity (hero, testimonials, thought leadership, news, brochures)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,39 @@ import JourneySection from "@/components/JourneySection";
 import StatsSection from "@/components/StatsSection";
 import ExpertiseSection from "@/components/ExpertiseSection";
 import AboutSection from "@/components/AboutSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
 import PartnersSection from "@/components/PartnersSection";
+import InsightsSection from "@/components/InsightsSection";
+import BrochuresSection from "@/components/BrochuresSection";
 import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
+import { getHomePageContent } from "@/sanity/queries";
 
-export default function Home() {
+export default async function Home() {
+  const { hero, testimonials, thoughtLeadership, newsItems, brochures } =
+    await getHomePageContent();
+
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero
+        heading={hero?.heading}
+        subheading={hero?.subheading}
+        ctaText={hero?.ctaText}
+        ctaLink={hero?.ctaLink}
+        backgroundImage={hero?.backgroundImage ?? null}
+      />
       <JourneySection />
       <StatsSection />
       <ExpertiseSection />
       <AboutSection />
+      <TestimonialsSection testimonials={testimonials} />
       <PartnersSection />
+      <InsightsSection
+        thoughtLeadership={thoughtLeadership}
+        newsItems={newsItems}
+      />
+      <BrochuresSection brochures={brochures} />
       <CTASection />
       <Footer />
     </>
